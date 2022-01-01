bcc63c076f5927e735d57b45a8dfbd096b5e5197
David Wilson <david.craig.wilson@gmail.com>
2022-01-01 11:07:31 -0600

    attempting graphql

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -4,11 +4,25 @@ import { resolvers } from "./resolvers";
 import { MicroRequest } from "apollo-server-micro/dist/types";
 import { ServerResponse } from "http";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { print } from 'graphql';
+
+class BasicLogging {
+  requestDidStart({queryString, parsedQuery, variables}: any): void {
+    const query = queryString || print(parsedQuery);
+    console.log(query);
+    console.log(variables);
+  }
+
+  willSendResponse({graphqlResponse}: any) {
+    console.log(JSON.stringify(graphqlResponse, null, 2));
+  }
+}
 
 const apolloServer = new ApolloServer({ 
     typeDefs, 
     resolvers,  
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground()], 
+    extensions: [() => new BasicLogging()]
 });
 
 export const config = {
